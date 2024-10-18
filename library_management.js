0976a4d49b029ea5750aa3bb50fd7fe67bdbe178
manujunqueira <manuelajunqueira27@gmail.com>
2024-10-18 12:09:54 -0400

    “Create Book class with properties and methods”

--- library_management.js
+++ library_management.js
@@ -1,2 +1,29 @@
 // Task 1 - Create a Book Class.
 
+class Book {
+    constructor(title, author, ISBN){
+        this.tittle = tittle; 
+        this.author = author;
+        this.ISBN = ISBN;
+        this._isAvailable = true; // pattern: available 
+    } // created "book" class
+
+getDetails(){
+    return `${this.tittle} by ${this.author}, ISBN: ${this.ISBN}.`;
+    } // returning the book's details
+
+get isAvailable(){ // getter for book availability. returns true if available 
+    return this._isAvailable;
+    }
+
+set isAvailable(status){ // setter to change book availability 
+    this._isAvailable = status;
+    }
+
+};
+
+// example
+
+const book1 = new Book("Moby-Dick", "Herman Melville", "1234567890"); // adding a book
+console.log (book1.getDetails()); // log the details and availability 
+console.log (book1._isAvailable); // "true"
